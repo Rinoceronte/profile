42ef3bee96893fc1208b1a76ffb84c883f632098
Thomas <thomas.crockett24@gmail.com>
2021-04-21 09:07:25 -0600

    fixed so will run on server

--- server/index.js
+++ server/index.js
@@ -3,10 +3,11 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+app.use(express.static(__dirname + '/../build'));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '../build/index.html'))
-});
 
-app.listen(3005, () => console.log('running on 3005'));
-//will it add?
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, "../build/index.html"));
+  });
+
+app.listen(3005, () => console.log('running on 3005', path.join(__dirname, "../build/index.html")));
